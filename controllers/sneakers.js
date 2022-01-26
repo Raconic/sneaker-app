d631a96763311c83382a9ffcfb19adc55e9fa9d2
username <youremail@domain.com>
2022-01-25 16:00:44 -0800

    touch up

--- controllers/sneakers.js
+++ controllers/sneakers.js
@@ -18,17 +18,18 @@ function index(req, res) {
     });
   }
 
-function create(req, res) {
-    console.log(req.body);
-    // The model is responsible for creating data
-    Sneaker.create(req.body);
-    // Do a redirect anytime data is changed
-    res.redirect('/');
-}
 
 function newSneaker (req, res) {
     res.render('sneakers/new', );
   }
 
-
-
+  function create(req, res) {
+    const sneaker = new sneaker(req.body);
+    // Assign the logged in user's id
+    sneaker.userRecommending = req.user._id;
+    sneaker.save(function(err) {
+      if (err) return render();
+      // Probably want to go to newly added book's show view
+      res.redirect(`/sneakers/${sneaker._id}`);
+    });
+  }
